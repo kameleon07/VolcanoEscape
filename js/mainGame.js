8c2f7e2b97ddb8753280cb026c720a8ddf57dbb8
kameleon07 <37112322+kameleon07@users.noreply.github.com>
2018-04-16 00:27:18 +0100

    asd

--- js/mainGame.js
+++ js/mainGame.js
@@ -82,7 +82,7 @@ function switchLevel(icey) {
 		lavaColour2 = 0x8490f0;
 		lavaColour3 = 0x3e4fcc;
 		scene.fog = new THREE.Fog(0xd6dbd6, 0, 200);
-		fogColour2 = 0xffffff;
+		scene.fog = new THREE.Fog(0xffffff, 0, 60);
 	} else {
 		floorColour = 0xd6dbd6;
 		floorColour2 = 0x000000;
