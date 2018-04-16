983462ce34d22ebd50b0da12ec5c5a5b38d13983
kameleon07 <37112322+kameleon07@users.noreply.github.com>
2018-04-16 12:30:15 +0100

    cd

--- js/mainGame.js
+++ js/mainGame.js
@@ -792,7 +792,7 @@ function animate() {
 }
 function displayScore()
 {
-	showFinalScore =document.getElementById("muteBtn");
+	showFinalScore =document.getElementById("boi");
 	showFinalScore.style.background = "url(images/score.png) no-repeat";
 //document.getElementById("boi").innerHTML = '<p id = "finalScore"></p>';
 //document.getElementById("boi2").innerHTML = score;
