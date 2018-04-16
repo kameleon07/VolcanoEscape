bac62de24fd5a0feb3000f5689ac3bbab7ee5bd4
kameleon07 <37112322+kameleon07@users.noreply.github.com>
2018-04-16 01:14:43 +0100

    asdasd

--- js/mainGame.js
+++ js/mainGame.js
@@ -94,7 +94,7 @@ function switchLevel(icey) {
 		lavaColour2 = 0x38a536;
 		lavaColour3 = 0x22ce1e;
 		scene.background = new THREE.Color(0x9aa897);
-		scene.fog = new THREE.Fog(0xd6dbd5, 0, 200);
+		scene.fog = new THREE.Fog(0x859583, 0, 200);
 		fogColour2 = 0x32e02c;
 	}
 	// Generate new floor geometry object
